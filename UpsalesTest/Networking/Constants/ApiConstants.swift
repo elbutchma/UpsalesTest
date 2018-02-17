94136a4bbe94e520cdb0abaa17bbf187e55cd27f
Mostafa Elbutch <elbutchma@gmail.com>
2018-02-17 20:22:05 +0200

    Created Api constants file.

---
+++ UpsalesTest/Networking/Constants/ApiConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ApiConstants.swift
+//  UpsalesTest
+//
+//  Created by Mostafa Elbutch on 2/17/18.
+//  Copyright © 2018 Mostafa Elbutch. All rights reserved.
+//
+
+import Foundation
+
+class ApiConstants {
+    static let baseURl = "https://integration.upsales.com/api/v2/"
+    static let apiToken = "cbc1f4ec352883d57a43c7611d3461d36ada467ead3ba22d08af439ad843cb50"
+    static let accountsAPI = "accounts"
+    static let filtersAPI = "users"
+}
